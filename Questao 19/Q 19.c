f72288313c55272f15209b947c7907f4f6977c12
helderdantas <37123238+helderdantas@users.noreply.github.com>
2020-10-12 23:15:11 -0300

    Update and rename Q 19.c to Questao 19/Q 19.c

--- Questao 19/Q 19.c
+++ Questao 19/Q 19.c
@@ -2,16 +2,16 @@
 #include <assert.h>
 #include <stdio.h>
 
-int main ()
+int main (void)
 {
   int i, **p, **r;
 
-  GC_INIT (); / * Opcional no Linux / X86; ver abaixo. * /
-  para (i = 0; i <60000000; ++ i)
+  GC_INIT ();
+  for (i = 0; i <60000000; ++ i)
    {
      int ** p = (int **) GC_MALLOC (sizeof (int *));
      int * r = (int *) GC_MALLOC_ATOMIC (sizeof (int));
-     afirmar (* p == 0);
+     assert(* p == 0);
      * p = (int *) GC_REALLOC (r, 2 * sizeof (int));
      if (i% 200000 == 0)
        printf ("Tamanho do heap =% d \ n", GC_get_heap_size ());
