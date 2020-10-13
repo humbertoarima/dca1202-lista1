07065a8abc07026f83107be37944f733c75a4cbb
helderdantas <37123238+helderdantas@users.noreply.github.com>
2020-10-12 23:18:47 -0300

    Update Q 19.c

--- questao 19/Q 19.c
+++ questao 19/Q 19.c
@@ -6,12 +6,12 @@ int main ()
 {
   int i, **p, **r;
 
-  GC_INIT (); / * Opcional no Linux / X86; ver abaixo. * /
+  GC_INIT (); / * testar no linux * /
   para (i = 0; i <60000000; ++ i)
    {
      int ** p = (int **) GC_MALLOC (sizeof (int *));
      int * r = (int *) GC_MALLOC_ATOMIC (sizeof (int));
-     afirmar (* p == 0);
+     resset (* p == 0);
      * p = (int *) GC_REALLOC (r, 2 * sizeof (int));
      if (i% 200000 == 0)
        printf ("Tamanho do heap =% d\n", GC_get_heap_size ());
